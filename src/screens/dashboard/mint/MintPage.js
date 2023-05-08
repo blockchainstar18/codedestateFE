7f036fad12f7ff58b5e8417c0e6f3226d9d2e96b
blockchainstar18 <Fedirpiddu@outlook.com>
2023-05-08 17:44:43 +0300

    paused

--- src/screens/dashboard/mint/MintPage.js
+++ src/screens/dashboard/mint/MintPage.js
@@ -150,7 +150,7 @@ function MintPage() {
                             <div style={{ "width": "6px", "height": "6px", "borderRadius": "3px", "background": "green", "marginTop": "10px" }}></div>
                             <div>Status</div>
                         </div>
-                        <div className='fw-bold fs-5'>Open</div>
+                        <div className='fw-bold fs-5'>Paused</div>
                     </div>
                     <div className='flex-column'>
                         <div className='d-flex flex-row'>
